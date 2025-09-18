b89861fcb2abdeb5f74ecc0218a9ffc539446ce2
gc04346 <gc04346@gmail.com>
2025-09-18 18:45:00 -0300

    fix: icones pwa

--- vite.config.ts
+++ vite.config.ts
@@ -19,17 +19,17 @@ export default defineConfig({
         start_url: '/DeltaCash---Organizador-Financeiro/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: "/DeltaCash---Organizador-Financeiro/pwa-192x192.png",
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: "/DeltaCash---Organizador-Financeiro/pwa-512x512.png",
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: "/DeltaCash---Organizador-Financeiro/pwa-512x512.png",
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
